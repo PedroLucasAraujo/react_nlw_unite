55527e80ad02259b495c8a6506b30529359b4bb2
PedroLucasAraujo <pedrolpriv@gmail.com>
2024-04-02 21:25:26 -0300

    feat: create homepage layout

---
+++ src/components/header.tsx
@@ -0,0 +1,14 @@
+import nlwUniteIcon from "../assets/nlw-unite-icon.svg"
+
+export function Header() {
+    return (
+        <header className="flex items-center gap-5">
+            <img src={nlwUniteIcon} alt="NLW Unite" />
+
+            <nav className="flex items-center gap-5 py-2">
+                <a className="font-medium text-sm text-zinc-300" href="">Eventos</a>
+                <a className="font-medium text-sm" href="">Participantes</a>
+            </nav>
+        </header>
+    )
+}
